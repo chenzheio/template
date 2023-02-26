dcc71266785795ca1237b6edc645d740efb0d674
chenzheio <github@chenzhe.dev>
2023-02-26 19:02:17 +0800

    Add JWT authentication and local strategy

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,30 @@
+import { Loaded } from '@mikro-orm/core';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compareSync } from 'bcrypt';
+import User from 'src/user/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+    ) { }
+
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.userService.findByUsername(username);
+        if (user && compareSync(password, user.password)) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+    async login(user:Loaded<User>){
+        const payload = { username: user.username, sub: user.id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+}
---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFormRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET
+        })
+    }
+
+    async validate(payload: { username: string; sub: string; }) {
+        return {
+            id: payload.sub,
+            username: payload.username
+        }
+    }
+}
